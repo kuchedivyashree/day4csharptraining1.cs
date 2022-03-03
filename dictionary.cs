fba0f2b7b2e61e7eaff4241a05ab8187ca8d50a0
003BCI744 <003BCI744@IBM-PF3CXDFH>
2022-03-03 11:30:01 +0530

    Add project files.

---
+++ dictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class dictionary
+    {
+        public static void Main(string[] args)
+        {
+            IDictionary<int, int> d = new Dictionary<int, int>();
+            d.Add(1, 97);
+            d.Add(2, 89);
+            d.Add(3, 77);
+            d.Add(4, 88);
+
+            Console.WriteLine("dictionary elements :" + d.Count);
+            Console.ReadLine();
+
+        }
+    }
+}
